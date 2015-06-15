0da013df72d5efed1b8ce1c1de1d354adc0bfc1c
katie Pekarek <katie_pekarek@yahoo.com>
2015-06-14 20:29:16 -0600

    constructor practice

--- instanceArray.js
+++ instanceArray.js
@@ -1,5 +1,5 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
@@ -13,29 +13,36 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+var users = [];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
-
-  //code here
+var tyler = new User("Tyler", "tylermcginnis33@gmail.com", 'iLoveJS');
+var cahlan = new User("Cahlan", "cahlan@devmounta.in", 'iLoveHashtags');
+var lenny = new User("Lenny", "lenny@theLenster.com", 'iLoveLentilSoup')
+users.push(tyler, cahlan, lenny)
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+console.log(tyler.name + ' ' + tyler.email + ' ' + tyler.pw);
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
-  //code here
+console.log(lenny.name + ' ' + lenny.email + ' ' + lenny.pw);
 
 
 //Now create another instance of User using your own information and then add that to your users array.
+var jimmy = new User("Jimmy", "jim@usmail.com", 'youloveme')
+user.push(jimmy);
 
-  //code here
+console.log('Jimmy\'s information is ');
+
+console.log(jimmy.name + ' ' + jimmy.email + ' ' + jimmy.pw);
 
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
+users.forEach(function(user){console.log(user.name);})
 
   //code here
